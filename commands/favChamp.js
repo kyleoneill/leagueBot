138b25ceb22166f2e91e0dd2fd4838bee48680dc
Kyle O'Neill <koneill@rationalenterprise.com>
2018-07-26 13:59:14 -0400

    Swapped better-sqlite3 for sqlite3

--- commands/favChamp.js
+++ commands/favChamp.js
@@ -3,31 +3,31 @@ const getTime = common.getTime
 
 module.exports = {
     name:'favChamp',
-    execute(message, args) {
+    async execute(message, args) {
         try {
             if(!args) {
-                message.channel.send(`You're missing arguments. Use 'set' to set a favorite champion or 'get' to see yours.`)
+                await message.channel.send(`You're missing arguments. Use 'set' to set a favorite champion or 'get' to see yours.`)
             }
             if(args[0] == 'set') {
                 if(!args[1]) {
-                    message.channel.send(`You haven't told me what to set your champion to be.`)
+                    await message.channel.send(`You haven't told me what to set your champion to be.`)
                     return
                 }
-                var truncatedInput = args[1].substring(0, 500)
-                this.botDatabase.set(message, truncatedInput)
-                message.channel.send(`I've set your favorite champion to be ${truncatedInput}, ${message.author.username}.`)
+                var truncatedInput = await args[1].substring(0, 500)
+                await this.botDatabase.set(message, truncatedInput)
+                await message.channel.send(`I've set your favorite champion to be ${truncatedInput}, ${message.author.username}.`)
             }
             else if(args[0] == 'get') {
-                var info = this.botDatabase.get(message)
-                if(!info) {
-                    message.channel.send(`You haven't set any data yet.`)
-                    return
-                }
-                var {favChamp} = info
-                message.channel.send(`${message.author.username}, your favorite champion is ${unescape(favChamp)}.`)
+                var info = await this.botDatabase.get(message, function(data){
+                    if(!data) {
+                        message.channel.send(`You haven't set any data yet.`)
+                        return
+                    }
+                    message.channel.send(`${message.author.username}, your favorite champion is ${unescape(data)}`)
+                })
             }
             else {
-                message.channel.send(`That isn't a valid argument. See '!help' for assistance.`)
+                await message.channel.send(`That isn't a valid argument. See '!help' for assistance.`)
             }
         }
         catch(e) {
--- src/databases/userDB.js
+++ src/databases/userDB.js
@@ -1,37 +1,45 @@
 const puppeteer = require('puppeteer')
 const common = require('../common')
 const timeStamp = common.getTime
-const sqlite = require('better-sqlite3')
-const sql = new sqlite('./database.sqlite')
+const sqlite = require('sqlite3').verbose()
+const db = new sqlite.Database('./database.sqlite')
 
 class dbFunctions {
     start() {
         try{
-            const table = sql.prepare(`SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'users';`).get();
-            if (!table['count(*)']) {
-                // If the table isn't there, create it and setup the database correctly.
-                sql.prepare(`CREATE TABLE users (id TEXT PRIMARY KEY, username TEXT, favChamp TEXT);`).run();
-                // Ensure that the "id" row is always unique and indexed.
-                sql.prepare(`CREATE UNIQUE INDEX idx_users_id ON users (id);`).run();
-                sql.pragma("synchronous = 1");
-                sql.pragma("journal_mode = wal");
-            }
+            db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name = 'users'`, (error, row) => {
+                if(row !== undefined) {
+                    console.log(`${timeStamp()}: SQLite table exists.`)
+                }
+                else {
+                    console.log(`${timeStamp()}: SQLite table does not exist, creating a new table.`)
+                    db.run("CREATE TABLE users(username TEXT, favChamp TEXT)")
+                }
+            })
         }
         catch(e) {
             console.log(`${timeStamp()}: ${e}`)
         }
     }
-    set(message, champName) {
+    async set(message, champName) {
         try{
-            sql.prepare(`INSERT OR REPLACE INTO users VALUES ('${message.author.id}-${message.guild.id}', '${message.author.username}', '${escape(champName)}')`).run()
+            await db.prepare(`INSERT OR REPLACE INTO users VALUES ('${message.author.username}', '${escape(champName)}')`).run()
         }
         catch(e) {
             console.log(`${timeStamp()}: ${e}`)
         }
     }
-    get(message) {
+    async get(message, callback) {
         try {
-            return sql.prepare(`SELECT * FROM users WHERE username == '${message.author.username}'`).get()
+            //var info
+            await db.get(`SELECT * FROM users WHERE username == '${message.author.username}'`, async (error, row) => {
+                try{
+                    callback(row.favChamp)
+                }
+                catch(e){
+                    callback(null)
+                }
+            })
         }
         catch(e) {
             console.log(`${timeStamp()}: ${e}`)
