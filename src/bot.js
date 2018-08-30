5ff6ac444fc376c7a94b4ea9a78116acb8ede746
Kyle <kyleoneill24@gmail.com>
2018-08-30 17:08:38 -0400

    Made bot-on-message async and added an await to the command execution to avoid a race condition when shutdown is used

--- src/bot.js
+++ src/bot.js
@@ -35,11 +35,10 @@ this.discordID = auth.discordID
 this.guildList = bot.guilds
 
 //On discord message
-bot.on('message', message => {
+bot.on('message', async message => {
     if(message.author.bot || !message.content.startsWith(config.prefix) || !message.guild) return
     const args = message.content.slice(config.prefix.length).split(', ') //Discard prefix, return command and arguments in an array. Vars are split by commas
     const command = args.shift() //Removes first var from args (the command) and stores it in const 'command'
-    var text = message.content
 
     if(!bot.commands.has(command)) {
         message.channel.send(`I don't seem to know '!${command}'. Check out '!help' to see what I can do.`)
@@ -51,8 +50,9 @@ bot.on('message', message => {
             logMessage += ` with args '${args}'`
         }
         console.log(logMessage)
-        bot.commands.get(command).execute.call(this, message, args)
+        await bot.commands.get(command).execute.call(this, message, args)
         if(command == 'shutdown' && message.author.username == 'sammie287') {
+            console.log(`${getTime()}: Bot shutting down.`)
             bot.destroy()
         }
     }
