bff8117b71a7f0406272f5785418ca35c204245d
Kyle <kyleoneill24@gmail.com>
2018-09-17 20:04:47 -0400

    Added emailLog method in common.js which is called on bot startup. Sends me an email including the log file. Installed pm2 module to keep bot running if it crashes for whatever reason.

--- src/common.js
+++ src/common.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const https = require('https')
+const nodemailer = require('nodemailer')
+const auth = require('../config/auth.json')
+const encoding = 'utf8'
 module.exports = {
     getTime: function() {
         var timeStamp = (new Date()).toLocaleString('en-US')
@@ -38,16 +41,53 @@ module.exports = {
         const path = 'log.txt'
         try{
             if(!fs.existsSync(path)){
-                fs.writeFileSync(path, '', 'utf8', (err) => {
+                fs.writeFileSync(path, '', encoding, (err) => {
                     if(err) throw err
                 })
             }
-            fs.appendFileSync(path, output, 'utf8', (err) => {
+            fs.appendFileSync(path, output, encoding, (err) => {
                 if(err) throw err
             })
         }
         catch(e){
             console.log(`${module.exports.getTime()}: Error saving log file.\n${e}`)
         }
+    },
+    emailLog: function(){
+        const path = 'log.txt'
+        var emailBody = null
+        if(!fs.existsSync(path)){
+            fs.writeFileSync(path, '', encoding, (err) => {
+                if(err) throw err
+            })
+            emailBody = "Log file not found."
+        }
+        else {
+            emailBody = fs.readFileSync(path, 'utf8')
+        }
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+            user: auth.botEmail,
+            pass: auth.botPass
+            }
+        })
+        
+        var mailOptions = {
+            from: auth.botEmail,
+            to: auth.myEmail,
+            subject: 'leagueBot Log',
+            text: `Automatically generated email from leagueBot`,
+            attachments: {filename:'log.txt',path:'log.txt'}
+        }
+        
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+            console.log(`Error Sending Email\n${error}`)
+            } else {
+            console.log('Email sent: ' + info.response)
+            module.exports.botLog(`Log file emailed successfully`)
+            }
+        })
     }
 }
