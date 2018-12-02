dbf38fa29bfd7597b69bf96abd989bf383a3e365
Kyle <kyleoneill24@gmail.com>
2018-12-02 17:24:31 -0500

    Fixed bug causing mastery command to fail for champions with teo word names

--- src/common.js
+++ src/common.js
@@ -17,6 +17,16 @@ module.exports = {
         var timeStamp = (new Date()).toLocaleString('en-US')
         return timeStamp
     },
+    titleCase: function(str){
+        var splitStr = str.toLowerCase().split(' ');
+        for (var i = 0; i < splitStr.length; i++) {
+            // You do not need to check if i is larger than splitStr length, as your for does that for you
+            // Assign it back to the array
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+        }
+        // Directly return the joined string
+        return splitStr.join(' '); 
+    },
     httpsGetAsync: function(url) {
         return new Promise((resolve, reject) => {
             https.get(url, (response) => {
