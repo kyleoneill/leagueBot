1ff2ef3e0676ec6783b873b9d70164da462cef23
Kyle O'Neill <koneill@rationalenterprise.com>
2019-01-15 15:55:54 -0500

    Removed redundant statement in user activity

--- src/bot.js
+++ src/bot.js
@@ -27,7 +27,7 @@ bot.on('ready', async () => {
     try{
         botLog(`~~~~~Bot Started~~~~~`)
         botLog(`Logged in as: ${bot.user.username}`)
-        await bot.user.setActivity("Playing Garen jungle");
+        await bot.user.setActivity("Garen jungle");
         await userDB.start()
     }
     catch(e){
