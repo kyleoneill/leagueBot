6de67b5f1160cb94b6d4fcf970440fb4c6d7c828
Kyle <kyleoneill24@gmail.com>
2019-01-22 22:36:37 -0500

    Updated riot API from deprecated v3 to v4

--- src/getRequests.js
+++ src/getRequests.js
@@ -1,12 +1,13 @@
 const https = require('https')
 const {leagueKey} = require('../config/auth.json')
+const common = require('./common')
 module.exports = {
     httpsGetAsync: function(url) {
         return new Promise((resolve, reject) => {
             https.get(url, (response) => {
                 let statusCode = response.statusCode
                 if(statusCode != 200){
-                    module.exports.botLog(`Status code ${statusCode} from API call.`)
+                    common.botLog(`Status code ${statusCode} from API call.`)
                     reject(statusCode)
                     return null
                 }
@@ -20,31 +21,36 @@ module.exports = {
             }).on('error', reject)
         })
     },
+    //SUMMONER-V4 - /lol/summoner/v4/summoners/by-name/{summonerName}
     getSummonerByName: async function(summonerName) {
         var summonerNameRegex = /^[a-z0-9 _.]+$/i
         var NameIsValid = summonerNameRegex.test(summonerName)
         if(NameIsValid) {
-            var summoner = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}?api_key=${leagueKey}`)
+            var summoner = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${leagueKey}`)
             return summoner
         }
         else {
             return null
         }
     },
+    //CHAMPION-V3 - /lol/platform/v3/champion-rotations
     getCurrentFreeChampions: async function() {
         var freeChampionList = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${leagueKey}`)
         return freeChampionList
     },
+    //CHAMPION-MASTERY-V4 - /lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}
     getChampionMasteryBySummonerID: async function(summonerID) {
-        var mastery = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/${summonerID}?api_key=${leagueKey}`)
+        var mastery = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${summonerID}?api_key=${leagueKey}`)
         return mastery
     },
+    //CHAMPION-MASTERY-V4 - /lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}/by-champion/{championId}
     getSpecificChampionMastery: async function(accountID, championID) {
-        var mastery = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/${accountID}/by-champion/${championID}?api_key=${leagueKey}`)
+        var mastery = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${accountID}/by-champion/${championID}?api_key=${leagueKey}`)
         return mastery
     },
+    //LEAGUE-V4 - /lol/league/v4/positions/by-summoner/{encryptedSummonerId}
     getRankingBySummoner: async function(summonerID) {
-        var ranking = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/${summonerID}?api_key=${leagueKey}`)
+        var ranking = await this.httpsGetAsync(`https://na1.api.riotgames.com/lol/league/v4/positions/by-summoner/${summonerID}?api_key=${leagueKey}`)
         return ranking
     }
 }
